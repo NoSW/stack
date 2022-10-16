832ba2ba728441946506f5b15dd008e4fd576918
NoSW <50603996+NoSW@users.noreply.github.com>
2022-10-16 18:57:36 +0800

    updated

---
+++ drafts/sync.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import shutil
+
+if __name__ == '__main__':
+    html_list = ""
+
+    for file in os.popen("git diff-index --cached --name-only HEAD").read().splitlines():
+        if file != "README.md" and file.endswith(".md"):
+            html_file = f"{file}.html"
+            shutil.copyfile(file, html_file)
+            with open(html_file, 'a') as f:
+                f.write("\n<style class=\"fallback\">body{visibility:hidden}</style><script>markdeepOptions={tocStyle:'long'};</script><!-- Markdeep: --><script src=\"https://casual-effects.com/markdeep/latest/markdeep.min.js?\" charset=\"utf-8\"></script>")
+            html_list += f" {html_file} "
+
+    if len(html_list) > 0:
+        os.system(f"git add {html_list}")
